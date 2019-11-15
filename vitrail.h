5f7b2919231867a7298fd4700996c8168ff87eba
genlanzer <glanz030@uottawa.ca>
2019-11-15 13:29:32 -0500

    Vitrail header

--- vitrail.h
+++ vitrail.h
@@ -1,7 +1,23 @@
+
+
 #ifndef VITRAIL_H
 #define VITRAIL_H
 
+#include <vector>
+using namespace std;
+
 class Vitrail {
-};
+    char **vitrail;
+
+    public:
+
+        int construireVitrail(std::vector<char>, int);
+        bool estComplete(int);
+        bool estEnConstruction(int);
+
+    };
+
+ostream& operator<<(ostream&, const Vitrail&) ;
+
 
 #endif // VITRAIL_H
