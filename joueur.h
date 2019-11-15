b01b59008c8291948aa3702523fa84b545af482e
gmonzerol@live.fr <gmonz035@uottawa.ca>
2019-11-15 13:30:06 -0500

    add class definitions

--- joueur.h
+++ joueur.h
@@ -1,15 +1,23 @@
 #ifndef JOUEUR_H
 #define JOUEUR_H
 #include <string>
+#include <iostream>
 #include "vitrail.h"
+
+using namespace std;
 
 class Joueur {
 private:
     std::string nom;
-    int points;
-    int position;
-    Vitrail* vitrail;
-
+    int         points;
+    int         position;
+    Vitrail*    vitrail;
+public:
+    Joueur&  operator++();
+    Joueur&  operator-=(const int);
+    Joueur&  operator~();
 };
+
+ostream& operator<<(ostream&, const Joueur&);
 
 #endif // JOUEUR_H
--- lots.h
+++ lots.h
@@ -1,7 +1,19 @@
 #ifndef LOTS_H
 #define LOTS_H
+#include <vector>
+#include <iostream>
+
+using namespace std;
 
 class Lots {
+private:
+    char         lot[4][5];
+    vector<char> surplus;
+public:
+    vector<char> ramasseVitre(char couleur, int numeroLot);
+    void reset();
 };
+
+ostream& operator<<(ostream&, const Lots&);
 
 #endif // LOTS_H
